51874a355c262d833319f86ba56b90fbd46be25b
hamzaqurbonov <77965943+hamzaqurbonov@users.noreply.github.com>
2023-12-28 22:34:05 +0500

    YouTubeFragment
    YouTubeFragment ishladi

--- app/src/main/java/com/example/xavfsizbolajon/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/xavfsizbolajon/ui/home/HomeFragment.java
@@ -1,37 +1,81 @@
 package com.example.xavfsizbolajon.ui.home;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.xavfsizbolajon.R;
 import com.example.xavfsizbolajon.databinding.FragmentHomeBinding;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.utils.YouTubePlayerUtils;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment  {
 
     private FragmentHomeBinding binding;
+    YouTubePlayerView youTubePlayerView;
+    private Button playNextVideoButton;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
+
+
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        youTubePlayerView = view.findViewById(R.id.youtube_player_view);
+//        playNextVideoButton = view.findViewById(R.id.next_video_button);
+        initYouTubePlayerView();
+
+        return view;
     }
+
+
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        // Checks the orientation of the screen
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            youTubePlayerView.matchParent();
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            youTubePlayerView.wrapContent();
+        }
     }
+
+
+    private void initYouTubePlayerView() {
+        getLifecycle().addObserver(youTubePlayerView);
+
+        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+
+
+                YouTubePlayerUtils.loadOrCueVideo(
+                        youTubePlayer, getLifecycle(),
+                        "X3tr5ax78V4", 0f
+                );
+            }
+        });
+
+    }
+
+
+
 }
