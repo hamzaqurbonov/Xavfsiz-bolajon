1b3c142983c6cf96d84a9ca08d01ee6c1b05106a
hamzaqurbonov <77965943+hamzaqurbonov@users.noreply.github.com>
2024-10-07 23:24:42 +0500

    Видеони add қилиш
    ишлади Fairbasega arrey map орқали сақлаш бўлди

--- app/src/main/java/com/example/xavfsizbolajon/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/xavfsizbolajon/ui/notifications/NotificationsFragment.java
@@ -4,33 +4,78 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.xavfsizbolajon.R;
 import com.example.xavfsizbolajon.databinding.FragmentNotificationsBinding;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class NotificationsFragment extends Fragment {
 
-    private FragmentNotificationsBinding binding;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    Map<String,Object> nestedData = new HashMap<>();
+    public List<String> activityllist = new ArrayList<>();
+    EditText edit_short_id;
+    Button add_button;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        NotificationsViewModel notificationsViewModel = new ViewModelProvider(this).get(NotificationsViewModel.class);
+        View view = inflater.inflate(R.layout.fragment_notifications, container, false);
 
-        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        edit_short_id = view.findViewById(R.id.edit_short_id);
+        add_button = view.findViewById(R.id.add_button);
 
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        AddButton();
+
+
+        return view;
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public void AddButton() {
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // видео IDсини олиш
+                String url = edit_short_id.getText().toString();
+                String videoId = url.substring(url.indexOf("/shorts/") + 8, url.indexOf("?"));
+
+                // Map яратиш
+                nestedData.put("url", edit_short_id.getText().toString());
+                nestedData.put("id",   videoId);
+
+                // arrey орқали сақлаш
+                DocumentReference Data = db.document("Shorts/boshqalar");
+                Data.update("key", FieldValue.arrayUnion(nestedData));
+                edit_short_id.setText("");
+
+                Toast.makeText(requireActivity(), "Видео сақланди", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
     }
+
+
+//    @Override
+//    public void onDestroyView() {
+//        super.onDestroyView();
+//        binding = null;
+//    }
 }
