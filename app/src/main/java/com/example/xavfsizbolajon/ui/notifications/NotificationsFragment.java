d2d46629a4db62d1b15107f3a4513c646cf9b1d8
hamzaqurbonov <77965943+hamzaqurbonov@users.noreply.github.com>
2024-10-08 18:31:43 +0500

    urllink
    сараланадиган қилинди

--- app/src/main/java/com/example/xavfsizbolajon/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/xavfsizbolajon/ui/notifications/NotificationsFragment.java
@@ -45,12 +45,13 @@ public class NotificationsFragment extends Fragment {
     public List<String> activityllist = new ArrayList<>();
     EditText edit_short_id;
     Button add_button;
-    Spinner spinner;
-    String DocName, videoId, url ;
+    Spinner spinner, spinner_young ;
+    String DocName,youngNumber, videoId, url ;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_notifications, container, false);
+        spinner_young = view.findViewById(R.id.spinner_young);
         spinner = view.findViewById(R.id.spinner);
         edit_short_id = view.findViewById(R.id.edit_short_id);
         add_button = view.findViewById(R.id.add_button);
@@ -104,39 +105,76 @@ public class NotificationsFragment extends Fragment {
                 // Хеч нарса танланмаса (ўрнатилган ҳолат)
             }
         });
+
+        // spinner_young ga tegishli
+        String[] spinner_young_list = {"1", "2", "3", "4", "5"};
+
+        ArrayAdapter<String> adapter_young = new ArrayAdapter<>(getActivity(),  android.R.layout.simple_spinner_item , spinner_young_list);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_young.setAdapter(adapter_young);
+        Log.d("demo46", "activityllist "  + activityllist);
+        spinner_young.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                // Танланган элементни олиш
+                String selectedOption = parentView.getItemAtPosition(position).toString();
+                youngNumber = selectedOption;
+                Toast.makeText(getActivity(), "Tanlangan: " + selectedOption, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // Хеч нарса танланмаса (ўрнатилган ҳолат)
+            }
+        });
+
+
+
+
     }
 
-    public void UrlLink() {
-        // видео IDсини олиш
-        url = edit_short_id.getText().toString();
-        String url_app_longV= url.substring(0, url.indexOf("/youtu.be/") + 10);
-        String url_app_shorts = url.substring(0, url.indexOf("/shorts/") + 8);
-        String url_pk_longV = url.substring(0, url.indexOf("v=") + 2);
-        String url_pk_shorts = url.substring(0, url.indexOf("/shorts/") + 8);
-
-        if (url_app_longV.equals("https://youtu.be/")) {
-             videoId = url.substring(url.indexOf("/youtu.be/") + 10, url.indexOf("?"));
-        } else if (url_app_shorts.equals("https://youtube.com/shorts/")) {
-             videoId = url.substring(url.indexOf("/shorts/") + 8, url.indexOf("?"));
-        } else if (url_pk_longV.equals("https://www.youtube.com/watch?v=")) {
-             videoId = url.substring(url.indexOf("v=") + 2);
-        } else if (url_pk_shorts.equals("https://www.youtube.com/shorts/")) {
-             videoId = url.substring(url.lastIndexOf("/") + 1);
-        } else {
-            Toast.makeText(requireActivity(), "Нотаниш линк киритилаяпти", Toast.LENGTH_SHORT).show();
-        }
-    }
 
     public void AddButton() {
         add_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Видео линкларини саралаш
-                UrlLink();
+
+
+                // Киритилаётган Видео линки
+                url = edit_short_id.getText().toString();
+
+                // Линкни аниқлаш олиш учун
+                String url_app_longV = "https://youtu.be/";
+                String url_app_shorts = "https://youtube.com/shorts/";
+                String url_pk_longV = "https://www.youtube.com/watch?v=";
+                String url_pk_shorts = "https://www.youtube.com/shorts/";
+
+                // Линкни филтирлаш учун
+                if (url.startsWith(url_app_longV)) {
+                    videoId = url.substring(url.indexOf("/youtu.be/") + 10, url.indexOf("?"));
+                } else if (url.startsWith(url_app_shorts)) {
+                    videoId = url.substring(url.indexOf("/shorts/") + 8, url.indexOf("?"));
+                } else if (url.startsWith(url_pk_longV)) {
+
+                    videoId = url.substring(url.indexOf("v=") + 2, url.indexOf("&") == -1 ? url.length() : url.indexOf("&"));
+                    if (url.length() == url.indexOf("v=") + 2 + videoId.length()) {
+                        // IDдан кейин бошқа белгилар йўқ
+                        videoId = url.substring(url.indexOf("v=") + 2);
+                    } else {
+                        // IDдан кейин белгилар мавжуд
+                        videoId = url.substring(url.indexOf("v=") + 2, url.indexOf("&"));
+                    }
+                } else if (url.startsWith(url_pk_shorts)) {
+                    videoId = url.substring(url.lastIndexOf("/") + 1);
+                } else {
+                    Toast.makeText(requireActivity(), "Фақат YouTube линкини киритиш зарур", Toast.LENGTH_SHORT).show();
+                    return; // Функцияни тугатади
+                }
 
                 // Map яратиш
                 nestedData.put("url", edit_short_id.getText().toString());
                 nestedData.put("id",   videoId);
+                nestedData.put("youngNumber",  youngNumber);
 
                 // arrey орқали сақлаш
                 DocumentReference Data = db.collection("Shorts").document(DocName);
